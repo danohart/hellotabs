1bde1f174e88fb70986a37a4e5c7c97f66f0b2ae
Daniel Hart <me@danielhart.co>
2021-11-01 10:59:05 -0500

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    // colors: {
+    //   // purple: {
+    //   //   light: "#7D5EBF",
+    //   //   DEFAULT: "#6C51A6",
+    //   //   dark: "#580259",
+    //   // },
+    //   // orange: {
+    //   //   light: "#F27405",
+    //   //   DEFAULT: "#F25C05",
+    //   // },
+    // },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
