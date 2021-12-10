f037d14922d58bd8a9a0f7aaea8224e238e0ad0a
Daniel Hart <me@danielhart.co>
2021-12-10 09:47:51 -0600

    Typo, class > className

--- components/Loader.js
+++ components/Loader.js
@@ -1,11 +1,11 @@
 export default function Loader() {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div class='font-bold text-4xl flex flex-row'>
+      <div className='font-bold text-4xl flex flex-row'>
         Get ready
-        <div class='animate-[pulse_1s_ease-in-out_infinite]'>.</div>
-        <div class='animate-[pulse_1.5s_ease-in-out_infinite]'>.</div>
-        <div class='animate-[pulse_2s_ease-in-out_infinite]'>.</div>
+        <div className='animate-[pulse_1s_ease-in-out_infinite]'>.</div>
+        <div className='animate-[pulse_1.5s_ease-in-out_infinite]'>.</div>
+        <div className='animate-[pulse_2s_ease-in-out_infinite]'>.</div>
       </div>
     </div>
   );
