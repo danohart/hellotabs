d7334b307e20a9891abcff0142eb018ef39054dc
Daniel Hart <me@danielhart.co>
2023-08-29 11:02:35 -0500

    Add redirects for old neighborhood url

--- next.config.js
+++ next.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/neighborhood",
+        has: [
+          {
+            type: "query",
+            key: "n",
+            // the page value will not be available in the
+            // destination since value is provided and doesn't
+            // use a named capture group e.g. (?<page>home)
+            value: "(?<neighborhood>.*)",
+          },
+        ],
+        permanent: true,
+        destination: "/neighborhood/:neighborhood",
+      },
+    ];
+  },
+};
+
+// https://www.hellochicago.co/neighborhood?n=Logan%20Square
+// https://www.hellochicago.co/neighborhood/Logan%20Square
