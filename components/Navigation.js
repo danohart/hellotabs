073811d21d38ab072633f0e5e3c889ed3c3b9330
Sean MacMullan <sean.macmullan@gmail.com>
2024-01-11 22:53:04 -0600

    Hide neighborhood filter by default

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import neighborhoods from "../lib/neighborhoods";
 
 export default function Navigation() {
-  let [showNeighborhoods, setShowNeighborhoods] = useState(true);
+  let [showNeighborhoods, setShowNeighborhoods] = useState(false);
 
   return (
     <div>
@@ -12,7 +12,7 @@ export default function Navigation() {
           className='m-1 w-3/4 bg-purple-500 text-white font-bold py-2 px-4 rounded dark:bg-purple-800'
           onClick={() => setShowNeighborhoods(!showNeighborhoods)}
         >
-          {showNeighborhoods ? "Hide Neighborhoods" : "Show Neighborhoods"}
+          {showNeighborhoods ? "Hide Neighborhoods" : "Find by Neighborhood"}
         </button>
       </div>
       <div
