fbe16a45d3ba7161bf7c716aaf21d92ce6927904
Sean MacMullan <sean.macmullan@gmail.com>
2024-01-14 18:48:43 -0600

    Filter place list to show current happy hours first.

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,22 @@ function Home() {
   let [amountOfPlaces, setAmountOfPlaces] = useState(10);
 
   const day = getDay();
-
-  const { data, error } = useSWR("/api/places/" + day, fetcher);
-
+  
   function showMorePlaces() {
     setAmountOfPlaces((amountOfPlaces += 10));
   }
-
+  
+  const { data, error } = useSWR("/api/places/" + day, fetcher);
   if (error) return <div>Failed to load</div>;
   if (!data) return <Loader />;
+  if (!data.success) return <div>Failed to load</div>;
+  let places = data.places;
 
-  const places = data.places;
+  // sort so active happy hours are first
+  let activeSpecialsPlaces = places.filter((place) => hasActiveHappyHour(place, day));
+  let otherPlaces = places.filter((place) => !hasActiveHappyHour(place, day));
+  places = [...activeSpecialsPlaces, ...otherPlaces];
+
 
   function happyHoursRightNow() {
     const currentTime = new Date();
@@ -101,3 +106,25 @@ function Home() {
 }
 
 export default Home;
+
+
+function getCurrentTime() {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+
+  // Format the current time as "HHMM"
+  const currentTime = hours * 100 + minutes;
+  return currentTime;
+}
+
+function isBetweenTwoTimes(startTime, endTime) {
+  const currentTime = getCurrentTime();
+  return currentTime >= startTime && currentTime <= endTime;
+}
+
+function hasActiveHappyHour(place, day) {
+  const dayInfo = place.day.filter((specialDay) => specialDay.name == day)[0];
+  const isHappeningNow = isBetweenTwoTimes(dayInfo.timeOfDay.startTime, dayInfo.timeOfDay.endTime);
+  return isHappeningNow;
+}
