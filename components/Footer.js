47ef7926f527eb9402afc6d53e9332a80f45c457
Daniel Hart <me@danielhart.co>
2024-01-24 16:18:46 -0600

    Update Creative Commons license

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className='text-sm w-3/4 mx-auto text-center py-12'>
-      <a rel='license' href='http://creativecommons.org/licenses/by-nc-nd/4.0/'>
+      <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
         <img
           alt='Creative Commons License'
-          src='https://i.creativecommons.org/l/by-nc-nd/4.0/80x15.png'
+          src='https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png'
           className='mx-auto'
           width='80'
           height='15'
@@ -14,9 +14,9 @@ export default function Footer() {
       </a>
       <br />
       This work is licensed under a{" "}
-      <a rel='license' href='http://creativecommons.org/licenses/by-nc-nd/4.0/'>
-        Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
-        International License
+      <a rel='license' href='http://creativecommons.org/licenses/by-nc-sa/4.0/'>
+        Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+        License
       </a>
       .
     </footer>
